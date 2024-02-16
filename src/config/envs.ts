ef62fdc1168809c3f819044be142ef54278cfa77
ChaparroAgustin <chaparroagustin91@hotmail.com>
2024-02-16 16:26:44 -0300

    a

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,10 +4,8 @@ import {get} from 'env-var';
 
 export const envs = {
 
-
     PORT: get('PORT').required().asPortNumber(),
     PUBLIC_PATH: get('PUBLIC_PORT').default('public').asString(),
-
 
 }
 
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -17,9 +17,6 @@ export class TodoRoutes {
         router.post('/',todoController.createTodo);
         router.put('/:id',todoController.updateTodo);
         router.delete('/:id',todoController.deleteTodo);
-        
-
-
 
         return router;
     }
